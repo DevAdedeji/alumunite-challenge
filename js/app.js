5a800f607f67e83e74a76556178424197ca3ad56
DevAdedeji <adedejitewogbade2@gmail.com>
2022-06-29 19:38:10 +0100

    Working on adding anither set of form

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ let schools = [
         name: '',
         stillSchooling: false,
         date: null,
+    },
+    {
+        name: '',
+        stillSchooling: false,
+        date: null,
     }
 ]
 
@@ -10,44 +15,48 @@ const formContainer = document.querySelector(".school-form")
 
 const addSchoolBtn = document.querySelector(".add-school")
 
-insertForm()
-
-// function insertForm(){
-//     schools.forEach((school)=>{
-//         const schoolEl = document.createElement("div")
-//         schoolEl.classList.add("school")
-//         schoolEl.innerHTML = `
-//         <div class="school">
-//                             <!-- Select school -->
-//                             <div class="form">
-//                                 <label for="school">Select a school you graduated from</label>
-//                                 <select name="school" id="${school.name}">
-//                                     <option value="Farm Academy">Farm Academy</option>
-//                                     <option value="Dominion Academy">Dominion Academy</option>
-//                                     <option value="Lead Academy">Lead Academy</option>
-//                                     <option value="Convenant Academy">Convenant Academy</option>
-//                                 </select>
-//                             <p>Can't find your school <a href="#">Add your school to the list</a> </p>
-//                             </div>
-//                             <!-- Still Schooling option -->
-//                             <div class="still-schooling">
-//                                 <input type="checkbox" name="still-schooling" id="${school.stillSchooling}">
-//                                 <p>Still schooling here presently</p>
-//                             </div>
-//                             <!-- Year Graduated -->
-//                             <div class="form">
-//                                 <label for="year">Year Graduated</label>
-//                                 <input type="date" name="year" id="year">
-//                             </div>
-//                         </div>
-//                         <div class="line">
-    
-//         </div>
+function insertForm(){
+    schools.forEach((school)=>{
+        const schoolEl = document.createElement("div")
+        schoolEl.classList.add("school")
+        schoolEl.innerHTML = `
+                        <!-- Select school -->
+                        <div class="form">
+                            <label for="school">Select a school you graduated from</label>
+                            <div>
+                                <select name="school" id="${school.name}">
+                                    <option value="Farm Academy">Farm Academy</option>
+                                    <option value="Dominion Academy">Dominion Academy</option>
+                                    <option value="Lead Academy">Lead Academy</option>
+                                    <option value="Convenant Academy">Convenant Academy</option>
+                                </select>
+                                <i class="fa fa-chevron-down" aria-hidden="true"></i>
+                            </div>
+                        <p>Can't find your school? <a href="#">Add your school to the list</a> </p>
+                        </div>
+                        <!-- Still Schooling option -->
+                        <div class="still-schooling">
+                            <input type="checkbox" name="still-schooling" id="${school.stillSchooling}">
+                            <p>Still schooling here presently</p>
+                        </div>
+                        <!-- Year Graduated -->
+                        <div class="form">
+                            <label for="year">Year Graduated</label>
+                            
+                                <input type="date" name="year" id="year">
+                            
+                        </div>
+                    <br/>
+                    <div class="line">
+                    </div>
+                    <br/>
                        
-//         `
-//         formContainer.prepend(schoolEl)
-//     })
-// }
+        `
+        formContainer.prepend(schoolEl)
+    })
+}
+
+insertForm()
 
 addSchoolBtn.addEventListener("click", ()=>{
     schools.push({
@@ -55,5 +64,4 @@ addSchoolBtn.addEventListener("click", ()=>{
         stillSchooling: false,
         date: null,
     })
-    insertForm()
 })
